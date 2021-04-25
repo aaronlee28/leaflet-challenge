ab648b6303d4c1d0a4cdc3b9d8c8dd72394ef7dc
aaron28 <aaron.lee28@outlook.com>
2021-04-25 16:34:12 +0700

    commit

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -21,7 +21,7 @@ var map = L.map("map-id", {
 
 d3.json(url, function(quakeData){
     console.log(quakeData);
-    function circleSize (magniture){
+    function markerSize (magniture){
         return magnitude * 3;
 };
     function chooseColor(depth){
@@ -43,9 +43,9 @@ d3.json(url, function(quakeData){
 
 L.geoJSON(quakeData, {
     layer: function (feature, coordinate){
-        return L.circle(coordinate,
+        return L.circleMarker(coordinate,
             {
-                radius: circleSize(feature.properties.mag),
+                radius: markerSize(feature.properties.mag),
                 fillColor: color(feature.geometry.coordinates[2]),
                 opacity: 0.5,
                 weight: 0.5,
