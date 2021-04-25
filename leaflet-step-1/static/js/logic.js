9410dc71cae3707d52537d6ff5a8a9ac580ec493
aaron28 <aaron.lee28@outlook.com>
2021-04-25 13:54:43 +0700

    commit

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -4,6 +4,8 @@ var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant
 // layerGroups for earthquake 
 var earthquakes = new.L.LayerGroup();
 
+function createMap(earthquakes) {
+
 // Create the tile layer that will be the background of our map 
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -19,6 +21,18 @@ var baseMaps = {
 
   // Create an overlayMaps object to hold the bikeStations layer
 var overlayMaps = {
-    "Bike Stations": bikeStations
+    "Earthquakes": Earthquakes
 };
 
+ // Create the map object with options
+var map = L.map("map-id", {
+    center: [40.73, -74.0059],
+    zoom: 3,
+    layers: [lightmap, bikeStations]
+});
+
+  // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+}).addTo(map);
+}
