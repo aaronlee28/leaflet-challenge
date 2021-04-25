e5a7ee1db7996ec4827d551726724954023cddcf
aaron28 <aaron.lee28@outlook.com>
2021-04-25 15:23:37 +0700

    commit

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,11 +1,11 @@
 // URL 
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
 // layerGroups for earthquake 
-var earthquakes = new.L.LayerGroup();
+var earthquakes = L.layerGroup();
 
 // Create the tile layer that will be the background of our map 
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "light-v10",
@@ -13,14 +13,14 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
 });
 
 // Create the map object with options
-var map = L.map("map-id", {
+var map = L.map("mapid", {
     center: [40.73, -74.0059],
     zoom: 3,
-    layers: [lightmap, earthquakes]
+    layers: [lightMap, earthquakes]
 });
 
 d3.json(url, function(quakeData){
-    function markerSize (magniture)
+    function markerSize (magniture){
         return magnitude * 4;
 };
     function chooseColor(depth){
@@ -53,10 +53,26 @@ L.geoJSON(quakeData, {
             }
         );
     },
-    
-})
-    
-)
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h3>Location: " + feature.properties.place + "</h3><hr><p>Date: "
+        + new Date(feature.properties.time) + "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+    }
+}).addTo(earthquakes);
+earthquakes.addTo(map);
 
+// Legend
+var legend = L.control({position: "bottomright"});
+legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend"),
+    depth = [-10, 10, 30, 50, 70, 90];
 
-
+    div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+for (var i =0; i < depth.length; i++) {
+    div.innerHTML += 
+    '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' +
+        depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+legend.addTo(map);
+});
