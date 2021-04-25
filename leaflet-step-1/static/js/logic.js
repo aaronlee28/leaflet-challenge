93bc4fdaa41d381f2593466f857b78831aa6e453
aaron28 <aaron.lee28@outlook.com>
2021-04-25 12:33:48 +0700

    commit

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -6,26 +6,12 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
     accessToken: API_KEY
 });
 
-// Initialize all of the LayerGroups we'll be using
-var layers = {
-    COMING_SOON: new L.LayerGroup(),
-    EMPTY: new L.LayerGroup(),
-    LOW: new L.LayerGroup(),
-    NORMAL: new L.LayerGroup(),
-    OUT_OF_ORDER: new L.LayerGroup()
-};
+
 
 // Create the map with our layers
 var map = L.map("map-id", {
     center: [40.73, -74.0059],
-    zoom: 12,
-    layers: [
-        layers.COMING_SOON,
-        layers.EMPTY,
-        layers.LOW,
-        layers.NORMAL,
-        layers.OUT_OF_ORDER
-    ]
+    zoom: 12
 });
 
 // Add our 'lightmap' tile layer to the map
